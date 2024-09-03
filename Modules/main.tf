76d50b7086cdeae4dacad9b2323c62a10c80489b
pranav-1244 <pprv.41@gmail.com>
2024-09-03 17:03:17 +0530

    Update main.tf

--- Modules/main.tf
+++ Modules/main.tf
@@ -1,1 +1,15 @@
-
+resource "null_resource" "apache" {
+  provisioner "local-exec" {
+    command = "${path.module}/../../scripts/apache-setup.sh"
+  }
+}
+resource "null_resource" "jenkins" {
+  provisioner "local-exec" {
+    command = "${path.module}/../../scripts/jenkins-setup.sh"
+  }
+}
+resource "null_resource" "sonarqube" {
+  provisioner "local-exec" {
+    command = "${path.module}/../../scripts/sonarqube-setup.sh"
+  }
+}
