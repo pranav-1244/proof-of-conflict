21b40cf561c45535ff21270d3085c94bf15ad6df
pranav-1244 <pprv.41@gmail.com>
2024-09-03 17:05:56 +0530

    Update jenkins-setup.sh

--- script/jenkins-setup.sh
+++ script/jenkins-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Updating and installing Jenkins..."
 sudo apt-get update
-sudo apt-get install -y openjdk-11-jdk
+sudo apt-get install -y openjdk-17-jdk
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
