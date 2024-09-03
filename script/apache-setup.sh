bf2a90065f5de21f25ae73a739c36af9bb890467
pranav-1244 <pprv.41@gmail.com>
2024-09-03 17:07:18 +0530

    Update apache-setup.sh

--- script/apache-setup.sh
+++ script/apache-setup.sh
@@ -2,6 +2,5 @@
 echo "Updating and installing Apache..."
 sudo apt-get update
 sudo apt-get install -y apache2
-echo "<html><body><h1>Hello World</h1></body></html>" | sudo tee /var/www/html/index.html
 sudo systemctl restart apache2
 
